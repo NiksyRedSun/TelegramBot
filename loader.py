0ba2423fcfee1242ace28987a838cb7561c72519
NiksyRedSun <112153281+NiksyRedSun@users.noreply.github.com>
2023-03-25 15:22:46 +0300

    GoodTemplate
    Some template for project I take from Latand. Thanks to this man for his course and his code

--- loader.py
+++ loader.py
@@ -1,8 +1,11 @@
+import os
+import dotenv
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from data import config
+dotenv.load_dotenv()
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+bot = Bot(token=os.getenv("token"), parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)

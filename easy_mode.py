e88757d5946570b4e9ebd650051461821bb07e19
NiksyRedSun <112153281+NiksyRedSun@users.noreply.github.com>
2023-03-28 21:02:18 +0300

    Full easy_mode

--- easy_mode.py
+++ easy_mode.py
@@ -1,14 +1,14 @@
 import dotenv
 import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor      # импорт екзекутера
+from aiogram.utils import executor
 from aiogram.dispatcher.filters.builtin import CommandStart, CommandHelp
 from aiogram.dispatcher.filters import Command
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ContentType
 
 dotenv.load_dotenv()
-bot = Bot(token=os.getenv("token"))                 # создание бота
-dp = Dispatcher(bot)                                # создаем диспатчер, передаем бота
+bot = Bot(token=os.getenv("token"))
+dp = Dispatcher(bot)
 
 
 @dp.message_handler(CommandStart())
@@ -21,7 +21,7 @@ async def bot_start(message: types.Message):
         ],
         resize_keyboard=True
     )
-    await message.answer(text="Стартуем", reply_markup=menu)
+    await message.answer(text="Ну че, ебаный в рот, погнали нахуй", reply_markup=menu)
 
 
 @dp.message_handler(CommandHelp())
@@ -36,21 +36,66 @@ async def bot_help(message: types.Message):
 
 @dp.message_handler(Command("menu"))
 async def show_menu(message: types.Message):
+    text = ("<b>Аккаунт менеджер</b>",
+            "/aqm_forming - Оформление",
+            "/admin_qns - Административные вопросы",
+            "",
+            "<b>Коллер</b>",
+            "/taking_calls - Прием звонков",
+            "/crm_systems - Ведение CRM системы",
+            "",
+            "<b>Менеджер вводного урока</b>",
+            "/objs - Отработка возражений",
+            "/inpl_forming - Оформление",
+            "",
+            "/marijuana"
+            )
+    await message.answer("\n".join(text), parse_mode="HTML")
 
-    await message.answer()
+
+@dp.message_handler(Command("aqm_forming"))
+async def aqm_forming(message: types.Message):
+    await message.answer(text="Здесь будет информация по оформлению")
 
 
-@dp.message_handler()                               # хэндлер, позволяет принимать текстовые сообщения
+@dp.message_handler(Command("admin_qns"))
+async def admin_qns(message: types.Message):
+    await message.answer(text="Здесь будет информация по административным вопросам")
+
+
+@dp.message_handler(Command("taking_calls"))
+async def taking_calls(message: types.Message):
+    await message.answer(text="Здесь будет информация по приему звонков")
+
+
+@dp.message_handler(Command("crm_systems"))
+async def crm_systems(message: types.Message):
+    await message.answer(text="Здесь будет информация по ведению CRM систем")
+
+
+@dp.message_handler(Command("objs"))
+async def objs(message: types.Message):
+    await message.answer(text="Здесь будет информация по отработке возражений")
+
+
+@dp.message_handler(Command("inpl_forming"))
+async def inpl_forming(message: types.Message):
+    await message.answer(text="Здесь будет информация по оформлению")
+
+
+@dp.message_handler(Command("marijuana"))
+async def marijuana(message: types.Message):
+    sticker = open("C:\\repos\\tg_bot\\mj.webp", "rb")
+    await bot.send_sticker(message.chat.id, sticker)
+
+
+@dp.message_handler()
 async def get_message(message: types.Message):
     chat_id = message.chat.id
     text = "Some_text"
-    # sent_message = await bot.send_message(chat_id=chat_id, text=text)       # await нужен, потому что все завязано на
-    await message.answer(text=text)     # асинхронности, send_message - метод, отправляет сообщение от бота,
-    # sent_message - нужен чтобы добраться до результата отправки сообщения
-    # await message.answer(text=text) как аналог await bot.send_message(chat_id=chat_id, text=text)
+    await message.answer(text=text)
 
 
 print("Если ты видишь это сообщение, значит бот в игре")
 
-executor.start_polling(dp)  # собирает все апдейты, чтобы их куда-то передавать, нужно прописать handler, для хэндлера
-# нужен dispatcher
+executor.start_polling(dp)

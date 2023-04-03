138069f78ae28c1fd4fb6894edac994bac9950b3
NiksyRedSun <112153281+NiksyRedSun@users.noreply.github.com>
2023-04-03 21:12:14 +0300

    SomeChanges

--- easy_mode.py
+++ easy_mode.py
@@ -10,18 +10,18 @@ dotenv.load_dotenv()
 bot = Bot(token=os.getenv("token"))
 dp = Dispatcher(bot)
 
+ids = [372178038, 218656239]
+
+def save_id(message):
+    global ids
+    if message.chat.id not in ids:
+        ids.append(message.chat.id)
+
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    menu = ReplyKeyboardMarkup(
-        keyboard=[
-            [
-            KeyboardButton(text="/menu")
-            ]
-        ],
-        resize_keyboard=True
-    )
-    await message.answer(text="Ну че, ебаный в рот, погнали нахуй", reply_markup=menu)
+    save_id(message)
+    await message.answer(text=f"Ну че, ебаный в рот, погнали нахуй {message.chat.id}")
 
 
 @dp.message_handler(CommandHelp())
@@ -34,66 +34,33 @@ async def bot_help(message: types.Message):
     await message.answer("\n".join(text))
 
 
-@dp.message_handler(Command("menu"))
+
+@dp.message_handler(Command("show_ids"))
 async def show_menu(message: types.Message):
-    text = ("<b>Аккаунт менеджер</b>",
-            "/aqm_forming - Оформление",
-            "/admin_qns - Административные вопросы",
-            "",
-            "<b>Коллер</b>",
-            "/taking_calls - Прием звонков",
-            "/crm_systems - Ведение CRM системы",
-            "",
-            "<b>Менеджер вводного урока</b>",
-            "/objs - Отработка возражений",
-            "/inpl_forming - Оформление",
-            "",
-            "/marijuana"
-            )
+    text = [str(i) for i in ids]
     await message.answer("\n".join(text), parse_mode="HTML")
 
 
-@dp.message_handler(Command("aqm_forming"))
-async def aqm_forming(message: types.Message):
-    await message.answer(text="Здесь будет информация по оформлению")
+
+@dp.message_handler(Command("send_spam"))
+async def spam(message):
+    global ids
+    for i in ids:
+        await bot.send_message(i, text="Spam")
 
 
-@dp.message_handler(Command("admin_qns"))
-async def admin_qns(message: types.Message):
-    await message.answer(text="Здесь будет информация по административным вопросам")
+@dp.message_handler(Command("menu"))
+async def bot_start(message: types.Message):
+    menu = ReplyKeyboardMarkup(
+        keyboard=[
+            [
+            KeyboardButton(text="/menu")
+            ]
+        ],
+        resize_keyboard=True
+    )
+    await message.answer(text="Ну че, ебаный в рот, погнали нахуй", reply_markup=menu)
 
-
-@dp.message_handler(Command("taking_calls"))
-async def taking_calls(message: types.Message):
-    await message.answer(text="Здесь будет информация по приему звонков")
-
-
-@dp.message_handler(Command("crm_systems"))
-async def crm_systems(message: types.Message):
-    await message.answer(text="Здесь будет информация по ведению CRM систем")
-
-
-@dp.message_handler(Command("objs"))
-async def objs(message: types.Message):
-    await message.answer(text="Здесь будет информация по отработке возражений")
-
-
-@dp.message_handler(Command("inpl_forming"))
-async def inpl_forming(message: types.Message):
-    await message.answer(text="Здесь будет информация по оформлению")
-
-
-@dp.message_handler(Command("marijuana"))
-async def marijuana(message: types.Message):
-    sticker = open("C:\\repos\\tg_bot\\mj.webp", "rb")
-    await bot.send_sticker(message.chat.id, sticker)
-
-
-@dp.message_handler()
-async def get_message(message: types.Message):
-    chat_id = message.chat.id
-    text = "Some_text"
-    await message.answer(text=text)
 
 
 print("Если ты видишь это сообщение, значит бот в игре")

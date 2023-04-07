7ecd9f364cd57120e5374f4fdfb0f0c029f19e8a
NiksyRedSun <112153281+NiksyRedSun@users.noreply.github.com>
2023-04-08 00:26:32 +0300

    Rate limit on attack
    Now we got rate_limit on attack
    So you can attack only one time in 0.75 sec

--- EasyGame.py
+++ EasyGame.py
@@ -9,17 +9,99 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ContentType
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.dispatcher.storage import FSMContext
+from aiogram.utils.exceptions import Throttled
+from aiogram.dispatcher.handler import CancelHandler, current_handler
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram.dispatcher import DEFAULT_RATE_LIMIT
+import asyncio
 import time
+from utils.misc import rate_limit
+
 
 
 def rate_limit(limit: int, key=None):
+    """
+    Decorator for configuring rate limit and key in different functions.
+
+    :param limit:
+    :param key:
+    :return:
+    """
+
     def decorator(func):
-        setattr(func, "throttling_rate_limit", limit)
+        setattr(func, 'throttling_rate_limit', limit)
         if key:
-            setattr(func, "throttling_key", key)
+            setattr(func, 'throttling_key', key)
         return func
+
     return decorator
 
+
+class ThrottlingMiddleware(BaseMiddleware):
+    """
+    Simple middleware
+    """
+
+    def __init__(self, limit=DEFAULT_RATE_LIMIT, key_prefix='antiflood_'):
+        self.rate_limit = limit
+        self.prefix = key_prefix
+        super(ThrottlingMiddleware, self).__init__()
+
+    async def on_process_message(self, message: types.Message, data: dict):
+        """
+        This handler is called when dispatcher receives a message
+
+        :param message:
+        """
+        # Get current handler
+        handler = current_handler.get()
+
+        # Get dispatcher from context
+        dispatcher = Dispatcher.get_current()
+        # If handler was configured, get rate limit and key from handler
+        if handler:
+            limit = getattr(handler, 'throttling_rate_limit', self.rate_limit)
+            key = getattr(handler, 'throttling_key', f"{self.prefix}_{handler.__name__}")
+        else:
+            limit = self.rate_limit
+            key = f"{self.prefix}_message"
+
+        # Use Dispatcher.throttle method.
+        try:
+            await dispatcher.throttle(key, rate=limit)
+        except Throttled as t:
+            # Execute action
+            await self.message_throttled(message, t)
+
+            # Cancel current handler
+            raise CancelHandler()
+
+    async def message_throttled(self, message: types.Message, throttled: Throttled):
+        """
+        Notify user only on first exceed and notify about unlocking only on last exceed
+
+        :param message:
+        :param throttled:
+        """
+        handler = current_handler.get()
+        dispatcher = Dispatcher.get_current()
+        if handler:
+            key = getattr(handler, 'throttling_key', f"{self.prefix}_{handler.__name__}")
+        else:
+            key = f"{self.prefix}_message"
+
+        # Calculate how many time is left till the block ends
+        delta = throttled.rate - throttled.delta
+
+        # Prevent flooding
+
+        # Sleep.
+        await asyncio.sleep(delta)
+
+        # Check lock status
+        thr = await dispatcher.check_key(key)
+
+        # If current message is not last with current key - do not send message
 
 
 class Unit:
@@ -125,6 +207,7 @@ dotenv.load_dotenv()
 bot = Bot(token=os.getenv("token"))
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+dp.middleware.setup(ThrottlingMiddleware())
 
 
 def save_id(message):
@@ -180,7 +263,7 @@ async def start_fight(message: types.Message, state: FSMContext):
     await message.answer(text=data.get("unit").fight_presentation(), reply_markup=menu)
 
 
-@rate_limit(limit=10)
+@rate_limit(limit=0.75)
 @dp.message_handler(Text("Атаковать"))
 async def attack(message: types.Message, state: FSMContext):
     data = await state.get_data()
@@ -203,10 +286,12 @@ async def attack(message: types.Message, state: FSMContext):
             text = "Злодей побежден " + ", ".join(alive_players) + " - при этом остались в живых\n" + ", ".join(death_players) + " - мертвы, да упокоятся их души"
             await message.answer(text=text)
             await restart_message(message)
+            await Test.Q2.set()
         else:
             text = "Похоже героям сегодня удалось победить злодея.\nВот они слева направо: " + ", ".join(alive_players)
             await message.answer(text=text)
             await restart_message(message)
+            await Test.Q2.set()
         return None
     await state.update_data(unit=unit)
     menu = ReplyKeyboardMarkup(
